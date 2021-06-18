3d69f072a6347cb471d5444322203289bca2cedc
Nguyen Thanh Tan <thanhtanpm2000@gmail.com>
2021-06-18 18:49:34 +0700

    add futa-system

---
+++ futa-system/futa-system/src/test/java/com/example/futasystem/FutaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
